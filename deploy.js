f3f9ceed6252995ba885ff7a2fc8bd900d63e04c
Stephen Schieberl <bantherewind@gmail.com>
2021-09-29 11:28:49 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,28 +1,20 @@
 const path = require("path");
 const exec = require("@actions/exec");
 
-let deploy = function (folder, bucket, distIds, invalidation) {
+let deploy = function (folder, bucket, distId, invalidation) {
   return new Promise((resolve, reject) => {
     try {
       const command = `npx s3-deploy@1.4.0 ./** \
-                          --bucket ${bucket} \
-                          --cwd . \
-                          --gzip xml,html,htm,js,css,ttf,otf,svg,txt`;
+                        --bucket ${bucket} \
+                        --cwd . \
+                        --distId ${distId} \
+                        --etag \
+                        --gzip xml,html,htm,js,css,ttf,otf,svg,txt \
+                        --invalidate "${invalidation}" \
+                        --noCache `;
 
-        const cwd = path.resolve(folder);
-        exec.exec(command, [], { cwd }).then(resolve).catch(reject);
-      distIds.forEach(distId => {
-        const command = `npx s3-deploy@1.4.0 \
-                          --cwd . \
-                          --distId ${distId} \
-                          --etag \
-                          --gzip xml,html,htm,js,css,ttf,otf,svg,txt \
-                          --invalidate "${invalidation}" \
-                          --noCache `;
-
-        const cwd = path.resolve(folder);
-        exec.exec(command, [], { cwd }).then(resolve).catch(reject);
-      });
+      const cwd = path.resolve(folder);
+      exec.exec(command, [], { cwd }).then(resolve).catch(reject);
     } catch (e) {
       reject(e);
     }
